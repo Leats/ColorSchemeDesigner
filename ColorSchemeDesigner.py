eec681c7f6c94bfcfc91b53d670ba09135e88bfd
leats <lea.tschiersch@fu-berlin.de>
2018-08-21 10:38:22 +0200

    changed how colors were saved from string to list of int for easier computing, added converter function to change a list of int to hex string

--- ColorSchemeDesigner.py
+++ ColorSchemeDesigner.py
@@ -6,44 +6,63 @@ split-complementary
 triadic
 tetradic
 monochrome
+
+Color is a list of 3 int values between 0 and 255
 """
 
 
 def complementary(color):
     """Gives the complementary color given one color saved as string.
     """
-    complementaryColor = ""
+    complementaryColor = []
 
     try:
         if not isinstance(color, str):
-            raise TypeError("color no string!")
+            raise TypeError("Color is no string")
         if not validColor(color):
-            raise ValueError("string not a valid color")
+            raise ValueError("String not a valid color")
     except (TypeError, ValueError) as e:
         print(e)
         exit()
     else:
-        complementaryColor += complementaryNumber(color[0:2])
-        complementaryColor += complementaryNumber(color[2:4])
-        complementaryColor += complementaryNumber(color[-2:])
+    	for number in color:
+    		complementaryColor.append(abs(number - 255))
         return(complementaryColor)
 
 
 def complementaryNumber(number):
-    """given a number in hex, returns the "complementary" number for colors"""
+    """Given a number in hex, returns the "complementary" number for colors."""
     comp = str(hex(abs(int(number, 16) - 255))[2:])
     if len(comp) < 2:
         comp = "0" + comp
     return comp
 
 
-def validColor(color):
-    """checks if given string is a valid color"""
-    validChars = "0123456789ABCDEF"
+def saturationChanger(color, percentage):
+	"""Changes the saturation of the given color to the given percentage.
+	Percentage should be between 0 and 100."""
+	pass
 
-    if len(color) == 6:
-        for char in color:
-            if char not in validChars:
+
+def validColor(color):
+    """Checks if given list is a valid color."""
+    if len(color) == 3:
+        for number in color:
+            if number < 0 or number > 255:
                 return False
         return True
     return False
+
+
+def toHex(color):
+	"""Changes a list containing three int values between 0 and 255 to the
+	corresponding hex color as a string."""
+	hexColor = ""
+
+	for number in color:
+		hexNumber = str(hex(abs(int(number, 16) - 255))[2:])
+	    if len(hexNumber) < 2:
+	    	# values might only be one digit long, this needs to be changed by a 0 in front
+	        hexNumber = "0" + hexNumber
+	    hexColor += hexNumber
+	return(hexColor)
